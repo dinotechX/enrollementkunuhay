3afab314d4636a6b933b21a274a0d1835f5764c9
Julian Angelo E. Paciente <89331725+dinotechX@users.noreply.github.com>
2023-06-06 02:34:50 +0800

    Add files via upload

---
+++ Enrollment/Enrollment/LOGIN.cs
@@ -0,0 +1,15 @@
+namespace Enrollment
+{
+    public partial class LOGIN : Form
+    {
+        public LOGIN()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Enrollment/Enrollment/SignUp.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace Enrollment
+{
+    partial class SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            FirstName = new TextBox();
+            LastName = new TextBox();
+            MiddleName = new TextBox();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            label9 = new Label();
+            label10 = new Label();
+            Gender = new TextBox();
+            label11 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            BirthPlace = new TextBox();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            comboPosition = new ComboBox();
+            Section = new TextBox();
+            label15 = new Label();
+            SchoolID = new TextBox();
+            label16 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Narrow", 27.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(114, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(502, 43);
+            label2.TabIndex = 5;
+            label2.Text = "SCHOOL ENROLLMENT SYSTEM";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Narrow", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(114, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(488, 33);
+            label1.TabIndex = 4;
+            label1.Text = "ST. LOUISE DE MARILLAC NONTESSORI";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.st_louis_cardinals_mlb_logo_sticker_mlb_04_5301_6e6cc7;
+            pictureBox1.Location = new Point(16, 16);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(92, 90);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Black", 27.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(934, 32);
+            label3.Name = "label3";
+            label3.Size = new Size(187, 52);
+            label3.TabIndex = 6;
+            label3.Text = "CREATE\r\n";
+            label3.TextAlign = ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(1027, 79);
+            label4.Name = "label4";
+            label4.Size = new Size(89, 32);
+            label4.TabIndex = 7;
+            label4.Text = "USER";
+            label4.TextAlign = ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial Narrow", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(16, 161);
+            label5.Name = "label5";
+            label5.Size = new Size(245, 31);
+            label5.TabIndex = 8;
+            label5.Text = "BASIC INFORMATION";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial Narrow", 15.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(22, 266);
+            label6.Name = "label6";
+            label6.Size = new Size(117, 25);
+            label6.TabIndex = 9;
+            label6.Text = "FIRST NAME";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial Narrow", 15.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(270, 266);
+            label7.Name = "label7";
+            label7.Size = new Size(112, 25);
+            label7.TabIndex = 10;
+            label7.Text = "LAST NAME";
+            // 
+            // FirstName
+            // 
+            FirstName.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            FirstName.Location = new Point(22, 234);
+            FirstName.Multiline = true;
+            FirstName.Name = "FirstName";
+            FirstName.Size = new Size(229, 29);
+            FirstName.TabIndex = 11;
+            // 
+            // LastName
+            // 
+            LastName.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            LastName.Location = new Point(270, 234);
+            LastName.Multiline = true;
+            LastName.Name = "LastName";
+            LastName.Size = new Size(229, 29);
+            LastName.TabIndex = 12;
+            // 
+            // MiddleName
+            // 
+            MiddleName.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            MiddleName.Location = new Point(520, 234);
+            MiddleName.Multiline = true;
+            MiddleName.Name = "MiddleName";
+            MiddleName.Size = new Size(229, 29);
+            MiddleName.TabIndex = 14;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Arial Narrow", 15.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(520, 266);
+            label8.Name = "label8";
+            label8.Size = new Size(133, 25);
+            label8.TabIndex = 13;
+            label8.Text = "MIDDLE NAME";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(767, 234);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(146, 29);
+            textBox1.TabIndex = 16;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Arial Narrow", 15.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(767, 266);
+            label9.Name = "label9";
+            label9.Size = new Size(73, 25);
+            label9.TabIndex = 15;
+            label9.Text = "SUFFIX";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Arial Narrow", 15.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.Location = new Point(934, 266);
+            label10.Name = "label10";
+            label10.Size = new Size(83, 25);
+            label10.TabIndex = 17;
+            label10.Text = "GENDER";
+            // 
+            // Gender
+            // 
+            Gender.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            Gender.Location = new Point(934, 234);
+            Gender.Multiline = true;
+            Gender.Name = "Gender";
+            Gender.Size = new Size(146, 29);
+            Gender.TabIndex = 18;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Arial Narrow", 15.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label11.Location = new Point(22, 351);
+            label11.Name = "label11";
+            label11.Size = new Size(201, 25);
+            label11.TabIndex = 19;
+            label11.Text = "BIRTHDATE MM/DD/YY";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(23, 325);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(228, 23);
+            dateTimePicker1.TabIndex = 21;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // BirthPlace
+            // 
+            BirthPlace.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            BirthPlace.Location = new Point(270, 319);
+            BirthPlace.Multiline = true;
+            BirthPlace.Name = "BirthPlace";
+            BirthPlace.Size = new Size(229, 29);
+            BirthPlace.TabIndex = 23;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Arial Narrow", 15.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label12.Location = new Point(270, 351);
+            label12.Name = "label12";
+            label12.Size = new Size(121, 25);
+            label12.TabIndex = 22;
+            label12.Text = "BIRTHPLACE";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Arial Narrow", 15.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label13.Location = new Point(32, 501);
+            label13.Name = "label13";
+            label13.Size = new Size(93, 25);
+            label13.TabIndex = 24;
+            label13.Text = "POSITION";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Arial Narrow", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label14.Location = new Point(23, 410);
+            label14.Name = "label14";
+            label14.Size = new Size(271, 31);
+            label14.TabIndex = 26;
+            label14.Text = "SCHOOL INFORMATION";
+            // 
+            // comboPosition
+            // 
+            comboPosition.FormattingEnabled = true;
+            comboPosition.Location = new Point(32, 475);
+            comboPosition.Name = "comboPosition";
+            comboPosition.Size = new Size(219, 23);
+            comboPosition.TabIndex = 27;
+            // 
+            // Section
+            // 
+            Section.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            Section.Location = new Point(32, 544);
+            Section.Multiline = true;
+            Section.Name = "Section";
+            Section.Size = new Size(229, 29);
+            Section.TabIndex = 29;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Arial Narrow", 15.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label15.Location = new Point(32, 576);
+            label15.Name = "label15";
+            label15.Size = new Size(87, 25);
+            label15.TabIndex = 28;
+            label15.Text = "SECTION";
+            // 
+            // SchoolID
+            // 
+            SchoolID.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            SchoolID.Location = new Point(270, 544);
+            SchoolID.Multiline = true;
+            SchoolID.Name = "SchoolID";
+            SchoolID.Size = new Size(229, 29);
+            SchoolID.TabIndex = 31;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Arial Narrow", 15.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label16.Location = new Point(270, 576);
+            label16.Name = "label16";
+            label16.Size = new Size(106, 25);
+            label16.TabIndex = 30;
+            label16.Text = "SCHOOL ID";
+            // 
+            // SignUp
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1149, 635);
+            Controls.Add(SchoolID);
+            Controls.Add(label16);
+            Controls.Add(Section);
+            Controls.Add(label15);
+            Controls.Add(comboPosition);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(BirthPlace);
+            Controls.Add(label12);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label11);
+            Controls.Add(Gender);
+            Controls.Add(label10);
+            Controls.Add(textBox1);
+            Controls.Add(label9);
+            Controls.Add(MiddleName);
+            Controls.Add(label8);
+            Controls.Add(LastName);
+            Controls.Add(FirstName);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "SignUp";
+            Text = "SignUp";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox FirstName;
+        private TextBox LastName;
+        private TextBox MiddleName;
+        private Label label8;
+        private TextBox textBox1;
+        private Label label9;
+        private Label label10;
+        private TextBox Gender;
+        private Label label11;
+        private DateTimePicker dateTimePicker1;
+        private TextBox BirthPlace;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private ComboBox comboPosition;
+        private TextBox Section;
+        private Label label15;
+        private TextBox SchoolID;
+        private Label label16;
+    }
+}
